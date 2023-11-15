3bfc539de1ff18a62d17d56c1d972f91d36b765d
Misha <awarebayes@gmail.com>
2023-11-15 16:43:12 +0300

    orig to scripts and requirements

--- scripts/mesh_to_orig.py
+++ scripts/mesh_to_orig.py
@@ -3,10 +3,8 @@ import json
 import numpy as np
 import cv2
 import pycolmap
+import argparse
 
-metadata_path = '/home/mscherbina/Documents/sfm_deploynmnets/office_vhq/static/storage/reconstructions/global/sdfstudio-data/meta_data.json'
-colmap_path = '/home/mscherbina/Documents/sfm_deploynmnets/office_vhq/static/storage/reconstructions/global/outputs/ref/'
-metadata = json.load(open(metadata_path))
 
 def filter_points(rec: pycolmap.Reconstruction):
     bbs = rec.compute_bounding_box(0.01, 0.99)
@@ -29,13 +27,19 @@ def filter_points(rec: pycolmap.Reconstruction):
     return xyzs, colors
 
 
+metadata_path = "/home/mscherbina/Documents/sfm_deploynmnets/office_vhq/static/storage/reconstructions/global/sdfstudio-data/meta_data.json"
+colmap_path = (
+    "/home/mscherbina/Documents/sfm_deploynmnets/office_vhq/static/storage/reconstructions/global/outputs/ref/"
+)
+metadata = json.load(open(metadata_path))
+
 refined = pycolmap.Reconstruction(colmap_path)
 points_xyz, colors = filter_points(refined)
 points_xzy = points_xyz[:, [0, 1, 2]]
 swapped_point_cloud = o3d.geometry.PointCloud()
 swapped_point_cloud.points = o3d.utility.Vector3dVector(points_xzy)
 
-model = o3d.io.read_triangle_mesh('meshes/main_v2.ply')
+model = o3d.io.read_triangle_mesh("meshes/main_v2.ply")
 coordinate_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1)
 model.compute_vertex_normals()
 
@@ -48,18 +52,13 @@ model.compute_vertex_normals()
 
 # Convert the swapped NumPy array back to an Open3D point cloud
 
-worldtogt = np.array(metadata['worldtogt'])
+worldtogt = np.array(metadata["worldtogt"])
 
-transform = np.array([
-    [-0.,  1.,  0.,  0.],
-    [ 1.,  0., -0., -0.],
-    [-0., -0., -1.,  0.],
-    [ 0.,  0.,  0.,  1.]]
-)
+transform = np.array([[-0.0, 1.0, 0.0, 0.0], [1.0, 0.0, -0.0, -0.0], [-0.0, -0.0, -1.0, 0.0], [0.0, 0.0, 0.0, 1.0]])
 
 camera_poses = []
 xyz = []
-#for frame in metadata['frames']:
+# for frame in metadata['frames']:
 #    pose = np.array(frame['camtoworld'])
 #    t = pose[:3, 3]
 
@@ -69,4 +68,4 @@ model.transform(transform)
 o3d.io.write_triangle_mesh("./meshes/main_v2_orig.ply", model)
 
 o3d.visualization.draw_geometries([model, swapped_point_cloud, coordinate_frame, *camera_poses])
-#o3d.visualization.draw_geometries([model, *camera_poses])
+# o3d.visualization.draw_geometries([model, *camera_poses])
